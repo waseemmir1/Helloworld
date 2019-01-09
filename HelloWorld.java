365dc26666c1f63b4ce5a1525a56fd129f1413ca
Waseem Mir <waseemmir@Waseems-MacBook-Pro.local>
2019-01-10 00:00:02 +0400

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+   public static void main(String[] args) {
+      // Prints "Hello, World" in the terminal window.
+		for(int i=1; i<10;i++) {
+      System.out.println("Hello, World");
+}
+   }
+}
+
